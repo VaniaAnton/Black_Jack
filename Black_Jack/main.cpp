b7c1dc8a8e4ba38e822f2b3720dfbe083332aa34
VaniaAnton <antonivan1234567@gmail.com>
2023-05-16 19:48:05 +0300

    Initial Commit

---
+++ Black_Jack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Black_Jack
+//
+//  Created by Ваня Антонь on 2023-05-16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
