faede54497db09601cc56556ab94f0cdc60ec69e
VaniaAnton <antonivan1234567@gmail.com>
2023-05-23 20:12:22 +0300

    game

--- Black_Jack/Game.cpp
+++ Black_Jack/Game.cpp
@@ -51,93 +51,32 @@ void Game::endGame()
         std::cout  << "       a$$$$$$$$$$a" << std::endl;
         this->m_GameOver = true;
     }
-        
-    else {
-        std::cout << "We continue the game until someone wins\n";
+    else if (player1.getHand()>player2.getHand() && player1.getHand() <= 21){
+        this->m_GameOver = true;
+        std::cout << player1.getName()<< " win!\n";
+        std::cout <<player2.getName()<< " lose!\n";
     }
+    else if (player2.getHand()>player1.getHand() && player2.getHand() <= 21){
+        this->m_GameOver = true;
+        std::cout << player2.getName()<< " win!\n";
+        std::cout <<player1.getName()<< " lose!\n";
+    }
+        
 }
 
 void Game::update()
 {
-    std::cout << "Enter W to draw a card for both \nEnter A to draw a card for " <<player1.getName()<< "\nEnter D to draw a card for "<<player2.getName()<<"\nThe first person who is closer to 21 wins!\n";
-    std::cin >> this->m_command;
+    std::cout << "press ENTER to get two cards and start a game " <<'\n';
+    player1.randomCard(); player1.randomCard();  player2.randomCard();  player2.randomCard();
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //std::cin.ignore();
+    updatePlayer1();
 
-    switch (this->m_command) {
-        case 'w':
-        case 'W':
-            std::cout << player1.getName() << " Drew - " << player1.randomCard() << '\n';
-            std::cout << player2.getName() << " Drew - " << player2.randomCard() << '\n';
-
-            std::cout << std::endl;
-            std::cout << std::endl;
-
-            std::cout << player1.getName() << "'s Hand - " << player1.getHand() << '\n';
-            std::cout << player2.getName() << "'s Hand - " << player2.getHand() << '\n';
-
-            std::cout << std::endl;
-            std::cout << std::endl;
-
-            endGame();
-
-            std::cout << "Press [Enter] to continue...\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cin.ignore();
-            break;
-        case 'a':
-        case 'A':
-            std::cout << player1.getName() << " Drew - " << player1.randomCard() << '\n';
-            std::cout << player2.getName() << "  did not Draw a card "<<std::endl;
-
-            std::cout << std::endl;
-            std::cout << std::endl;
-
-            std::cout << player1.getName() << "'s Hand - " << player1.getHand() << '\n';
-            std::cout << player2.getName() << "'s Hand - " << player2.getHand() << '\n';
-
-            std::cout << std::endl;
-            std::cout << std::endl;
-
-            endGame();
-
-            std::cout << "Press [Enter] to continue...\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cin.ignore();
-            break;
-        case 'd':
-        case 'D':
-            std::cout << player1.getName() << " did not Draw a card" << '\n';
-            std::cout << player2.getName() << "  Drew -  "<<player2.randomCard()<<std::endl;
-
-            std::cout << std::endl;
-            std::cout << std::endl;
-
-            std::cout << player1.getName() << "'s Hand - " << player1.getHand() << '\n';
-            std::cout << player2.getName() << "'s Hand - " << player2.getHand() << '\n';
-
-            std::cout << std::endl;
-            std::cout << std::endl;
-
-            endGame();
-
-            std::cout << "Press [Enter] to continue...\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cin.ignore();
-            break;
-        default:
-            std::cout << "Not a valid command, please try again.\nPress [Enter] to continue...\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cin.ignore();
-            // for clearing console (works only on Windows)
-            // system("cls")
-            break;
-    }
 }
 void Game::updatePlayer1(){
-    std::cout << player1.getName()<< " Now is your turn, press W to draw a card " <<'\n';
+    std::cout <<player1.getName()<<" now is your turn, if you want to get one more press W ,\npress S to pass the turn to the next player \npress Q if you have enough cards and will not take more"<<'\n';
+    std::cout << player1.getName() << " your Hand - " << player1.getHand() << '\n';
     std::cin >> this->m_command;
     
     switch (this->m_command) {
@@ -148,12 +87,11 @@ void Game::updatePlayer1(){
             std::cout << std::endl;
             
             std::cout << player1.getName() << " your Hand - " << player1.getHand() << '\n';
-            
-            std::cout <<"If you want to get one more press W , else press S to pass the turn to the next player "<<'\n';
-            
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cin.ignore();
+            updatePlayer2();
+            break;
         case 's':
         case 'S':
             for (int i=0; i<10; i++) {
@@ -161,10 +99,15 @@ void Game::updatePlayer1(){
             }
             updatePlayer2();
             break;
+        case 'q':
+        case 'Q':
+            updatePlayer2();
+            break;
     }
 }
 void Game::updatePlayer2(){
-    std::cout << player2.getName()<< " Now is your turn, press W to draw a card " <<'\n';
+    std::cout <<player2.getName()<<" now is your turn, if you want to get one more press W ,\npress S to pass the turn to the next player \npress Q if you have enough cards and will not take more"<<'\n';
+    std::cout << player2.getName() << " your Hand - " << player2.getHand() << '\n';
     std::cin >> this->m_command;
     
     switch (this->m_command) {
@@ -176,13 +119,20 @@ void Game::updatePlayer2(){
             
             std::cout << player2.getName() << " your Hand - " << player2.getHand() << '\n';
             
-            std::cout <<"If you want to get one more press W , else press S to check who is winner "<<'\n';
-            
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cin.ignore();
+            updatePlayer1();
+            break;
         case 's':
         case 'S':
+            for (int i=0; i<10; i++) {
+                std::cout<<std::endl;
+            }
+            updatePlayer1();
+            break;
+        case 'q':
+        case 'Q':
             endGame();
             break;
     }
